633a4d239249a405d0f2b04039600c83e3ccb339
esullivan755 <eb.navillus@gmail.com>
2025-11-27 11:49:48 -0500

    tidying up

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import torch
 LR = 1e-4
 R = 8
 LORA_ALPHA = 16
+N_WAYS = 5
+K_SHOTS = 5
+Q_EXAMPLES = 10
 TARGET_MODULES = ["query_proj", "value_proj"]
 
 if __name__ == "__main__":
@@ -17,17 +20,16 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-v3-base")
     special_tokens = {"additional_special_tokens": ["[HEAD]", "[TAIL]"]}
     tokenizer.add_special_tokens(special_tokens)
+    train_data = few_shot_text("data/fewrel_train.json", tokenizer, n_ways=N_WAYS, k_shot=K_SHOTS, q_shot=Q_EXAMPLES)
+    val_data = few_shot_text("data/fewrel_val.json", tokenizer, n_ways=N_WAYS, k_shot=K_SHOTS, q_shot=Q_EXAMPLES)
+    lora_cfg = LoraConfig(r=R, lora_alpha=LORA_ALPHA, target_modules=["query_proj", "value_proj"], lora_dropout=0.1)
     
-    # train_data = few_shot_text("data/fewrel_train.json", tokenizer, n_ways=5, k_shot=5, q_shot=10)
-    # val_data = few_shot_text("data/fewrel_val.json", tokenizer, n_ways=5, k_shot=5, q_shot=10)
+    model = PEFT_Wrapper(ModelClass=None, ModelName="microsoft/deberta-v3-base", lora_config=lora_cfg)
+    metalearner = MetaLearner(model, Is_PEFT=True)
     
-    # lora_cfg = LoraConfig(r=8, lora_alpha=16, target_modules=["query_proj", "value_proj"], lora_dropout=0.1)
-    # model = PEFT_Wrapper(ModelClass=None, ModelName="microsoft/deberta-v3-base", lora_config=lora_cfg)
-    # metalearner = MetaLearner(model, Is_PEFT=True)
+    optimizer = torch.optim.Adam(metalearner.parameters(), lr=1e-4)
     
-    # optimizer = torch.optim.Adam(metalearner.parameters(), lr=1e-4)
-    
-    # meta_loop(train_data, val_data, tokenizer, metalearner, optimizer)
+    meta_loop(train_data, val_data, tokenizer, metalearner, optimizer)
 
 
 
